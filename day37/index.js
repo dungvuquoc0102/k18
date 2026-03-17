bc16099472cab5f31fa7694805c48b718c93f826
dungvuquoc0102 <dung.vuquoc0102@gmail.com>
2026-03-18 00:42:45 +0700

    Add day 37

---
+++ day37/index.js
@@ -0,0 +1,51 @@
+// localStorage.setItem("theme", "light");
+// const theme = localStorage.getItem("theme");
+
+// console.log(theme);
+
+// localStorage.setItem("settings", JSON.stringify({ theme: "dark", lang: "vi" }));
+// const name = '""';
+// localStorage.setItem("name", name);
+
+// localStorage.removeItem("name");
+// localStorage.clear();
+
+// sessionStorage.setItem("cart_step", "2");
+
+// document.cookie = "username=alice";
+
+// document.cookie = "token=abc123; max-age=5";
+
+// document.cookie = "theme=dark; expires=31 Dec 2026 23:59:59";
+
+// document.cookie = "session=xyz; httpOnly";
+
+// console.log(document.cookie);
+
+// const cookieString = document.cookie;
+// "theme=dark; session=xyz"
+// const cookies = cookieString.split("; ").reduce((acc, cookie) => {
+//   const [key, value] = cookie.split("=");
+//   acc[key] = value;
+//   return acc;
+// }, {});
+
+// console.log(cookies);
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+// Auth:
+// 1. Đăng nhập:
+// - Thông tin email, password lên server
+// - Server kiểm tra thông tin, nếu đúng email và password lưu trong DB,
+//  thì tạo ra 1 access token (JWT) và 1 refresh token (random string) rồi trả về cho client
+// - Client lưu access token và refresh token vào localStorage
+
+//2. Ở trang chủ:
+// - Client gửi request lấy thông tin user lên server kèm theo access token
+// - Server kiểm tra access token, nếu hợp lệ trả về thông tin user
+
+//3. Khi access token hết hạn:
+//
